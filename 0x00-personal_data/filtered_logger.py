c9736458b0b7cf1c8a802d6d7dd0c643e8ff5ed2
MoodyEG <moodyeg2011@yahoo.com>
2024-08-29 16:12:16 +0300

    Testing 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -12,3 +12,23 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(f"{field}=[^{separator}]*",
                          f"{field}={redaction}", message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Constructor """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Format method """
+        return filter_datum(self.fields, self.REDACTION,
+                            logging.Formatter.format(self, record),
+                            self.SEPARATOR)
