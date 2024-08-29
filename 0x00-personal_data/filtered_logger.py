e3364b63ac74d5ceaac9766575df79be00f4a6bb
MoodyEG <moodyeg2011@yahoo.com>
2024-08-29 17:25:12 +0300

    Testing 3.1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,7 +13,7 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     db_password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
     db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
     db_name = os.getenv("PERSONAL_DATA_DB_NAME")
-    return mysql.connector.connect(
+    return mysql.connector.connection.MySQLConnection(
         user=db_username,
         password=db_password,
         host=db_host,
