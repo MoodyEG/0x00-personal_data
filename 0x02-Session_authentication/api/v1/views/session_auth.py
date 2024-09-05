140f39c576478d32826211b3926e4f3f9bac1e78
MoodyEG <moodyeg2011@yahoo.com>
2024-09-05 05:58:49 +0300

    Logout, Task 8

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -5,19 +5,19 @@ from flask import abort, jsonify, request
 import os
 
 
-@app_views.route('/auth_session/login', methods=['POST'],
+@app_views.route("/auth_session/login", methods=["POST"],
                  strict_slashes=False)
 def login() -> str:
     """ POST /api/v1/auth_session/login """
-    email = request.form.get('email')
+    email = request.form.get("email")
     if email is None:
         return jsonify({"error": "email missing"}), 400
-    password = request.form.get('password')
+    password = request.form.get("password")
     if password is None:
         return jsonify({"error": "password missing"}), 400
     from models.user import User
     try:
-        user = User.search({'email': email})
+        user = User.search({"email": email})
     except Exception:
         return jsonify({"error": "no user found for this email"}), 404
 
@@ -30,3 +30,14 @@ def login() -> str:
     response = jsonify(user[0].to_json())
     response.set_cookie(os.getenv("SESSION_NAME"), session_id)
     return response
+
+
+@app_views.route("/auth_session/logout", methods=["DELETE"],
+                 strict_slashes=False)
+def logout() -> str:
+    """ DELETE /api/v1/auth_session/logout """
+    from api.v1.app import auth
+    if auth.destroy_session(request):
+        return jsonify({}), 200
+    else:
+        abort(404)
