2317e0ca666b50332b7e32824889f172f0246678
MoodyEG <moodyeg2011@yahoo.com>
2024-09-10 09:12:30 +0300

    Testing find_user 0.2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -42,13 +42,11 @@ class DB:
         """ Find user by email and password """
         try:
             user = self._session.query(User).filter_by(**kwargs).first()
-            if user is None:
-                raise NoResultFound
-            return user
-        except NoResultFound:
-            raise NoResultFound
-        except InvalidRequestError:
+        except Exception as e:
             raise InvalidRequestError
+        if user is None:
+            raise NoResultFound
+        return user
 
     def update_user(self, user_id: int, **kwargs: str) -> None:
         """ Update user """
